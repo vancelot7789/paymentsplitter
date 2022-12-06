ca4929f282dff86a3f341c0681a5413e11a1074a
Byron <byron@ByrondeMacBook-Pro.local>
2022-12-06 05:58:11 -0800

    remove track page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import { useLocation, Switch } from 'react-router-dom';
 import AppRoute from './utils/AppRoute';
 import ScrollReveal from './utils/ScrollReveal';
-import ReactGA from 'react-ga';
+
 
 // Layouts
 import LayoutDefault from './layouts/LayoutDefault';
@@ -11,12 +11,7 @@ import LayoutDefault from './layouts/LayoutDefault';
 import Home from './views/Home';
 
 // Initialize Google Analytics
-ReactGA.initialize(process.env.REACT_APP_GA_CODE);
 
-const trackPage = page => {
-  ReactGA.set({ page });
-  ReactGA.pageview(page);
-};
 
 const App = () => {
 
@@ -27,7 +22,6 @@ const App = () => {
     const page = location.pathname;
     document.body.classList.add('is-loaded')
     childRef.current.init();
-    trackPage(page);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location]);
 
